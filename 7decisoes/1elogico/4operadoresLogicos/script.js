890bd959690b72dfdaf84f26f2341103bde7d72e
LUCASANDRIESKIAMARAL <LUCASANDRIESKIAMARAL@TQR220388>
2026-07-09 17:20:35 -0300

    Atualização4

---
+++ 7decisoes/1elogico/4operadoresLogicos/script.js
@@ -0,0 +1,16 @@
+var variavel1 = 53;
+var variavel2 = true;
+var variavelTexto = "zero";
+var variavelNumero = "24";
+var variavelLogica = false;
+var variavelCurta = "texto curso";
+
+if (variavel2 && variavelNumero == 24) {
+ console.log("Retorno verdadeira");
+} else {
+    console.log("Retorno falso");
+}
+if (variavel1 > 50) {console.log(variavel1 + " é maior que 50"); }
+if (variavelTexto != "um") { console.log("Negação da variavel lógica é verdadeira"); }
+if (!variavelLogica) {console.log("Negação da variavel lógica é verdadeira"); }
+if (variavelTexto != variavel1) { console.log("Valor das variaveis é diferente"); }
