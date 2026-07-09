3a0d9be819bee538cdfd5661608215ae5698937d
LUCASANDRIESKIAMARAL <LUCASANDRIESKIAMARAL@TQR220388>
2026-07-09 15:12:24 -0300

    atualização2

---
+++ 5alertPrompt/script.js
@@ -0,0 +1,41 @@
+var variavel;
+
+variavel = prompt("Informe um valor para esta variavel: ");
+
+alert(variavel);
+
+/*
+
+Alert 
+
+Uma outra forma de apresentar informações para 
+o usuário em sua página web é através da função alert. 
+Esta função tem como objetivo, assim como a função console, 
+apresentar informações para o usuário, porém está irá abrir 
+uma pequena janela na sua página web apresentando o conteúdo 
+a ser exibido ao usuário. Não é uma prática muito aconselhável 
+utilizar alert em sites já publicados, normalmente a função nativa 
+alert é utilizada durante a etapa de desenvolvimento de um site. 
+Prompt Durante o processo de desenvolvimento muitas vezes 
+precisamos alterar repetidamente o valor de uma variável ou 
+queremos por muitas vezes simplesmente utilizar uma informação 
+diferente para testar algo em nosso código, então ao invés de 
+ficar alterando nosso código, utilizamos a função nativa prompt.
+Ao contrário da função console que tem como finalidade apresentar 
+informações no console do navegador, a função prompt tem como finalidade 
+solicitar informações do console do navegador. a  Semelhante com a função 
+alert, a função prompt irá apresentar uma pequena tela na sua
+página web, porém esta irá conter uma caixa de texto onde você 
+poderá informar um valor a ser enviado para o seu código. Veja 
+no exemplo, na linha 1 declaramos uma variável chamada variavel, 
+na linha 3 atribuímos a variavel o valor que irá retornar a utilizamos 
+a função nativa prompt, na linha 5 apresentamos na tela o valor da variavel 
+utilizando a função nativa alert. Experimente executar este código 
+em seu script.js, você verá que ao abrir a página inicial de nosso 
+exemplo aparecerá uma caixa branca no meio da tela com o valor que 
+passamos por parâmetro para a função prompt e um local onde você pode 
+digitar algo, logo abaixo verá dois botões OK e Cancelar, talvez dependendo 
+do idioma de seu computador, o texto nos botões poderá ser 
+diferente, mas a ordem sempre será a mesma, o primeiro botão 
+será para confirmar e enviar o valor que você digitou para seu 
+código e o segundo botão para cancelar a operação. */
---
+++ 6operadores/script.js
@@ -0,0 +1,24 @@
+var somaESubtracao = 5 + 5 -7;
+var subtracaoEMultiplicacao = 30 - 15 * 2;
+var multiplicacaoEExponenciacao = 2 * 2 ** 3;
+var agrupamentoEDivisao = (22 - 2) / 4;
+
+console.log(somaESubtracao);
+console.log(subtracaoEMultiplicacao);
+console.log(multiplicacaoEExponenciacao);
+console.log(agrupamentoEDivisao);
+
+
+/*
+
+Operadores
+
+Na linguagam JavaScript existem diversos tipos de operadores,
+estes operadores nos ajudam a economizar linhas de código
+e nos auxiliam durante o processo de desenvolvimento do nosso projeto.
+Operadores são utilizados na linguagem JavaScript para realização
+de diversas operações como por exemplo operações aritméticas, subtração
+e multiplicação, também são utilizados para realizar atribuições
+de valores, validações, agrupamentos e chamadas de funções, 
+que veremos mais para frente. Tabela de ordem e 
+precedência de operadores e sua associatividade: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Operator_precedence */
