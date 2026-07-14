5d4aac7982c379a49f20408e35befa55a8a2b9a2
LUCASANDRIESKIAMARAL <LUCASANDRIESKIAMARAL@TQR220388>
2026-07-14 15:26:05 -0300

    Atualização5

--- 7decisoes/1elogico/7Switch/script.js
+++ 7decisoes/1elogico/7Switch/script.js
@@ -2,18 +2,44 @@ var nome = "Luskinha";
 var emprego = "desenvolvedor";
 
 switch (emprego) {
-    case "policial":;
-    console.log(nome + " é uma policial.");
-    break;
+    case "policial":
+        console.log(nome + " é uma policial.");
+        break;
     case "cozinheiro":
         console.log(nome + " é uma cozinheira.");
         break;
-        case "desenvolvedor":
-            console.log(nomw + " é uma desenvoolvedora.");
-            break;
-            case "lutador":
-                console.log(nome + " é uma lutadora.")
-                break;
-                default;
-                console.log(nome + " trabalha em outra coisa.")
+    case "desenvolvedor":
+        console.log(nome + " é uma desenvolvedora.");
+        break;
+    case "lutador":
+        console.log(nome + " é uma lutadora.");
+        break;
+    default:
+        console.log(nome + " trabalha em outra coisa.");
 }
+
+if (emprego === "policial") {
+    console.log(nome + " é uma policial.");
+} else if (emprego === "cozinheiro") {
+    console.log(nome + " é uma cozinheira.");
+} else if (emprego === "desenvolvedor") {
+    console.log(nome + " é uma desenvolvedora.");
+} else if (emprego === "lutador") {
+    console.log(nome + " é uma lutadora.");
+} else {
+    console.log(nome + " trabalha em outra coisa.");
+}
+
+/*
+
+ Switch
+
+ No exemplo temos a variavel nome com o nome de uma pessoa
+  e uma outra variavel emprego com a informações
+ no que esta pessoa trabalha.
+
+ o case verifica se caso o valor da variavel emprego seja uma
+ das opcões ali declaradas como policial, então executara o codigo
+ logo abaixo.
+
+ */
